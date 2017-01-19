397a90af6e770d9d2fb1a0d1b74f5795ac1b1a52
Tim Taylor <ttaylor@clarion.edu>
2017-01-19 09:06:59 -0500

    passing any number returns 1

--- test_functions.rb
+++ test_functions.rb
@@ -1,8 +1,15 @@
 require 'minitest/autorun'
 
+def mined_minds_function(number)
+	1
+end
 
 class TestMMFunctions < Minitest::Test 
 	def test_one_equals_one()
 		assert_equal(1,1)
 	end
+
+def test_one_to_mined_minds_fucntion_returns_mined()
+		assert_equal(1, mined_minds_function(1))
+	end	
 end
